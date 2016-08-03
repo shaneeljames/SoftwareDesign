b66d4f34a4b6efc414efef05350300794e52e005
JaredTremayneNaidoo <jaredtremayne@gmail.com>
2016-08-03 22:29:13 +0200

    Async HTTP somewhat works
    Work on Async to be done

---
+++ FindMeTutor/FindMeTutor/app/src/main/java/com/example/jared/findmetutor/query.java
@@ -0,0 +1,94 @@
+package com.example.jared.findmetutor;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by jared on 2016/08/03.
+*/
+public class query extends AsyncTask<String , Void ,String> {
+
+    String server_response = "Failed";
+
+    public String ans(){
+
+        new query().execute("http://52.35.36.20/connect.php");
+        double j = 0;
+        while(j<1100000000){
+            j=j+0.5;
+        }
+        return server_response;
+    }
+
+
+    @Override
+    protected String doInBackground(String... strings) {
+
+        URL url;
+        HttpURLConnection urlConnection = null;
+
+        try {
+            url = new URL(strings[0]);
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            int responseCode = urlConnection.getResponseCode();
+
+            if(responseCode == HttpURLConnection.HTTP_OK){
+                server_response = readStream(urlConnection.getInputStream());
+                Log.v("CatalogClient", server_response);
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        Log.e("Response 4rm Log", "" + server_response);
+
+
+    }
+// Converting InputStream to String
+
+    private String readStream(InputStream in) {
+        BufferedReader reader = null;
+        StringBuffer response = new StringBuffer();
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return response.toString();
+    }
+
+}
+
