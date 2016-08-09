99218699108e0738afbe10d54ad1dccdeee3a8c1
Jadon Jesse <jadon_jesse@yahoo.com>
2016-08-09 16:36:06 +0200

    Login & Register working
    Changed the target sdk to 23 inorder to use Adroid's much better looking
    "Material design"
    Still havent connected to the the server , though all input are  stored
    as strings and ready to be passed into the login.java class
    Also updated the logo of our app system-wide... Thinking of removing the
    "findMeTutor" text under the logo and going with the straight logo  ...
    There are some other  minor changes changes....

---
+++ FindMeTutor/FindMeTutor/app/src/main/java/com/example/jared/findmetutor/LoginActivity.java
@@ -0,0 +1,200 @@
+package com.example.jared.findmetutor;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    //Global Variables to be used
+    private Toolbar toolbar;
+    private String name, email, password;
+    private EditText inputName, inputEmail, inputPassword;
+    private TextInputLayout inputLayoutName, inputLayoutEmail, inputLayoutPassword;
+    private Button btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //get and assign toolbar entered information
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //get inputLayouts
+        inputLayoutName = (TextInputLayout) findViewById(R.id.input_layout_name);
+        inputLayoutEmail = (TextInputLayout) findViewById(R.id.input_layout_email);
+        inputLayoutPassword = (TextInputLayout) findViewById(R.id.input_layout_password);
+
+        //get EditText objects
+        inputName = (EditText) findViewById(R.id.input_name);
+        inputEmail = (EditText) findViewById(R.id.input_email);
+        inputPassword = (EditText) findViewById(R.id.input_password);
+
+
+        btnSignUp = (Button) findViewById(R.id.btn_signup);
+        TextView btnRegister = (TextView) findViewById(R.id.tvRegister);
+
+        //use EditText objects to moniter the  Text fields for valid input
+        inputName.addTextChangedListener(new MyTextWatcher(inputName));
+        inputEmail.addTextChangedListener(new MyTextWatcher(inputEmail));
+        inputPassword.addTextChangedListener(new MyTextWatcher(inputPassword));
+
+        //get Strings from EditText objects
+        name = inputName.getText().toString();
+        email= inputEmail.getText().toString();
+        password = inputPassword.getText().toString();
+
+        //Submit form when login is clicked
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitForm();
+            }
+        });
+
+        //go to Register page if Register TextView Object is clicked
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent regAct = new Intent(LoginActivity.this, RegisterActivity.class);
+                //regAct.putExtra() send stuff to regActivity
+                LoginActivity.this.startActivity(regAct);
+            }
+        });
+
+    }// end on create
+
+    /**
+     * Validating form
+     */
+    //Validate entered information
+    private void submitForm() {
+
+        if (!validateName()) {
+            return;
+        }
+
+        if (!validateEmail()) {
+            return;
+        }
+
+        if (!validatePassword()) {
+            return;
+        }
+
+        if(validateName() && validateEmail() && validatePassword())
+        {
+            Toast.makeText(getApplicationContext(), "Thank You!", Toast.LENGTH_SHORT).show();
+
+        }
+
+
+    }
+    //Checks if name is a valid name if not return false and keep focus otherwise return true
+    private boolean validateName() {
+        if (inputName.getText().toString().trim().isEmpty()) {
+            inputLayoutName.setError(getString(R.string.err_msg_name));
+            requestFocus(inputName);
+            return false;
+        } else {
+            inputLayoutName.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validateEmail() {
+        String email = inputEmail.getText().toString().trim();
+
+        if (email.isEmpty() || !isValidEmail(email)) {
+            inputLayoutEmail.setError(getString(R.string.err_msg_email));
+            requestFocus(inputEmail);
+            return false;
+        } else {
+            inputLayoutEmail.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validatePassword() {
+        if (inputPassword.getText().toString().trim().isEmpty()) {
+            inputLayoutPassword.setError(getString(R.string.err_msg_password));
+            requestFocus(inputPassword);
+            return false;
+        } else {
+            inputLayoutPassword.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private static boolean isValidEmail(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    private class MyTextWatcher implements TextWatcher {
+
+        private View view;
+
+        private MyTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.input_name:
+                    validateName();
+                    break;
+                case R.id.input_email:
+                    validateEmail();
+                    break;
+                case R.id.input_password:
+                    validatePassword();
+                    break;
+            }
+        }
+    }
+
+
+
+
+    public void click(View view){
+        login j = new login(this, "jaredtremayne@gmail.com", "Tremayne852");
+        j.execute();
+        //register j = new register(this, "Jared","Naidoo", "0118229141", "Jared@gmail.com","19935050", "My dogs name?", "RUfus", "Student");
+        //j.execute();
+    }
+
+
+
+ }
